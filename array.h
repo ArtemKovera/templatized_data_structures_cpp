e7d24edece63b7a2a569a18295db1f53af8515a7
Artem Kovera <artemkovera@gmail.com>
2020-06-03 15:50:18 +0300

    added helper method clean and destructor to array.h

--- array.h
+++ array.h
@@ -16,6 +16,8 @@ public:
     //and the values of the array elements equal to the second argument
     Array(size_t, T);
 
+    ~Array();
+
     //this operator returns or sets an arrays element at given position which is the parameter for this operator
     T& operator[] (size_t);
 
@@ -29,13 +31,16 @@ private:
     //helper method for memory allocation
     //allocates memory for an array 
     //elements are zero initialized 
-    void memoryAlloc (size_t);
+    void memoryAlloc(size_t);
 
     //size of the array
     size_t size;
 
     //default size of the array created by default constructor
     static const size_t defaultSize = 100;
+
+    //helper method for freeing memory
+    void clean();
 };
 
 template<typename T>
@@ -64,6 +69,12 @@ Array<T>::Array(size_t s, T value)
 }
 
 template<typename T>
+Array<T>::~Array()
+{
+    clean();
+}
+
+template<typename T>
 void Array<T>::memoryAlloc(size_t s)
 {
     pointer = new T[s] {}; 
@@ -82,6 +93,13 @@ T& Array<T>::operator[](size_t index)
         throw std::out_of_range("");
 
     return pointer[index];
+}
+
+template<typename T>
+void Array<T>::clean()
+{
+    delete [] pointer;
+    pointer = nullptr;
 }
 
 
