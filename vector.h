80e9d517c693a6bfdf7b089a666b4151c1e20e5c
Artem Kovera <artemkovera@gmail.com>
2020-06-10 11:58:06 +0300

    added most pieces of functionality to vector.h

--- vector.h
+++ vector.h
@@ -1,11 +1,14 @@
 
 /*
   
-  !!!This templatized class works in a different way than the vector in STL!!!
+  !!! This templatized class works in a different way than the vector in STL !!!
 
 */
 
-#include<exception>
+#ifndef _VECTOR_H_
+#define _VECTOR_H_
+
+#include<stdexcept>
 #include<initializer_list>
 
 template<typename T>
@@ -24,41 +27,56 @@ public:
 
     //initializer-list constructor taking variable number of arguments
     Vector(std::initializer_list<T>);
+
+    virtual ~Vector();
+
+    Vector(const Vector&) = delete;
+
+    Vector& operator=(const Vector&) = delete;
+
+    Vector(Vector&&) = delete;
+
+    Vector& operator=(Vector&&) = delete;
     
     //inserts a new element at the end of a vector
     void pushBack(T);
     
     //returns the number of allocated members
     size_t getCapacity() const;
-
-    //changes the size of a vector
-    void resize(size_t)
     
     //this method returns true if the vector is empty and false otherwise
     bool isEmpty() const;
+
+    //this method returns true if the vector is full and false otherwise
+    bool isFull() const;
 
     //returns the number of vector elements
     size_t getSize() const;
 
     //this operator sets or returns an element of a vector
+    //if the vector is empty or the argument is equal or greater than the size, it throws out_of_range exception
     T& operator[](size_t);
     
     //this operator works with const objects
+    //if the vector is empty or the argument is equal or greater than the size, it throws out_of_range exception
     const T& operator[](size_t) const;
 
     //returns the first element of a vector
+    //if the vector is empty, it throws out_of_range exception
     const T& front() const;
 
     //returns the last element of a vector
+    //if the vector is empty, it throws out_of_range exception
     const T& back() const;
+
+    //this method sets the number of allocated members that equals to the argument of this method
+    //if the argument is less than the current size, it throws the invalid_argument exception
+    void reserve(size_t); 
 
 private:
     //pointer pointing to a vector
     T* pointer = nullptr;
     
-    //helper method for memory allocation
-    void allocateMemory(size_t);
-
     //the number of vector elements
     size_t size;
 
@@ -68,17 +86,23 @@ private:
     static const size_t defaultNumberOfAllocatedMembers = 100;
 
     //helper method for freeing memory
-    void clean() noexcept;
+    void clean(T*) noexcept;
+
+    //changes the size of a vector
+    void resize(size_t);
+
+    //helper method for memory allocation
+    void allocateMemory(size_t);        
 };
 
 template<typename T>
-Vector<T>::Vector(): size{0}, numberOfAllocatedMembers{defaultNumberOfAllocatedMembers}
+Vector<T>::Vector(): size{0}
 {
     allocateMemory(defaultNumberOfAllocatedMembers);
 }
 
 template<typename T>
-Vector<T>::Vector(size_t s): size{0}, numberOfAllocatedMembers{s}
+Vector<T>::Vector(size_t s): size{0}
 {
     allocateMemory(s);
 }
@@ -90,7 +114,7 @@ Vector<T>::Vector(size_t s, T val): size{s}, numberOfAllocatedMembers{s}
 }
 
 template<typename T>
-Vector<T>::Vector(std::initializer_list<T> args): size{args.size()}, numberOfAllocatedMembers{args.size()}
+Vector<T>::Vector(std::initializer_list<T> args): size{args.size()}
 {
     allocateMemory(args.size());
     
@@ -103,7 +127,133 @@ Vector<T>::Vector(std::initializer_list<T> args): size{args.size()}, numberOfAll
 }
 
 template<typename T>
+Vector<T>::~Vector()
+{
+    clean(pointer);
+}
+
+template<typename T>
 void Vector<T>::allocateMemory(size_t s)
 {
     pointer = new T[s];
+    numberOfAllocatedMembers = s;
 }
+
+template<typename T>
+void Vector<T>::pushBack(T val)
+{
+    if(size < (numberOfAllocatedMembers - 1))
+    {
+        pointer[size] = val;
+        size++;
+    }
+    else
+    {
+        resize(numberOfAllocatedMembers + 10);
+        pointer[size] = val;
+        size++;
+    }
+}
+
+template<typename T>
+void Vector<T>::resize(size_t s)
+{
+    T* ptr = pointer;
+    allocateMemory(s);
+    for(size_t i = 0; i<size; i++)
+        pointer[i] = ptr[i];
+    clean(ptr);
+}
+
+template<typename T>
+void Vector<T>::clean(T* ptr) noexcept
+{
+    delete [] ptr;
+    ptr = nullptr;
+}
+
+template<typename T>
+size_t Vector<T>::getCapacity() const
+{
+    return numberOfAllocatedMembers;
+}
+
+template<typename T>
+size_t Vector<T>::getSize() const
+{
+    return size;
+}
+
+template<typename T>
+const T& Vector<T>::front() const
+{
+    if(size == 0)
+        throw std::out_of_range("The vector is empty");
+
+    return pointer[0];
+}
+
+template<typename T>
+const T& Vector<T>::back() const
+{
+    if(size == 0)
+        throw std::out_of_range("The vector is empty");
+
+    return pointer[size-1];    
+}
+
+template<typename T>
+void Vector<T>::reserve(size_t s)
+{
+    if(s == size)
+        return;
+    
+    if(s < size)
+        throw std::invalid_argument("The size to reserve must not be less than the current size");
+
+    T* ptr = pointer;   
+    allocateMemory(s);
+    for(size_t i = 0; i < size; i++)
+        pointer[i] = ptr[i];
+
+    clean(ptr);
+}
+
+template<typename T>
+bool Vector<T>::isEmpty() const
+{
+    if(size == 0)
+        return true;
+    else
+        return false;
+}
+
+template<typename T>
+bool Vector<T>::isFull() const
+{
+    if(size == numberOfAllocatedMembers)
+        return true;
+    else
+        return false;
+}
+
+template<typename T>
+T& Vector<T>::operator[](size_t index)
+{
+    if(size == 0 || index >= size)
+        throw std::out_of_range("Invalid index");
+    
+    return pointer[index];
+}
+
+template<typename T>
+const T& Vector<T>::operator[](size_t index) const
+{
+    if(size == 0 || index >= size)
+        throw std::out_of_range("Invalid index"); 
+
+    return pointer[index];
+}
+
+#endif
+
